0467098687433fbb0df409a70f0311db1a1e389b
samsonjj <samsonjj@vcu.edu>
2019-02-18 09:44:08 -0500

    complete, needs comments

--- ngram.pl
+++ ngram.pl
@@ -1,8 +1,8 @@
 
 use strict;
 use warnings;
-
 use feature 'say';
+
 
 
 # Stores the tokens from the corpus, before inserting into the hash.
@@ -17,11 +17,30 @@ my @tokenArray = ();
 # So "hash" contains word histories as keys, and a hash of word/count pairs as values, which show the frequency of words occuring after a certain history.
 my %hash = ();
 
+# Look through command line arguments for a verbose tag.
+my $verbose = 0;
+my $argCount = scalar @ARGV;
+for( my $i=0; $i<$argCount; $i++) {
+  if( $ARGV[$i] eq "-v" ) {
+    $verbose = 1;
+    splice @ARGV, $i, 1;
+  }
+}
+
+# Get the number of command line arguments.
+$argCount = scalar @ARGV;
+
 # The size of the ngram model. If n=3 then the next word is based on the last 2 (n-1) words.
 my $n = 4;
+if($argCount > 0) {
+  $n = $ARGV[0];
+}
 
 # The number of sentences to generate.
 my $m = 10;
+if($argCount > 1) {
+  $m = $ARGV[1];
+}
 
 # Constants to represent the start and end of sentences when storing in the hash table.
 my $START_TAG = chr(219);
@@ -33,60 +52,76 @@ say '
   |  \| |______| |  __| |__) |   /  \  | \  / |
   | . ` |______| | |_ |  _  /   / /\ \ | |\/| |
   | |\  |      | |__| | | \ \  / ____ \| |  | |
-  |_| \_|       \_____|_|  \_\/_/    \_\_|  |_|
-                                                                                     
+  |_| \_|       \_____|_|  \_\/_/    \_\_|  |_|                                              
 ';
 
 say '  Written by Jonathan Samson';
-say "  For CMSC 416-001 VCU Spring 2019\n";
+say "  For CMSC 416-001 VCU Spring 2019\n\n";
 
-# Open the file
-my $inputFile = 'aviation-in-canada.txt';
+say "Command line settings : ngram.pl @ARGV";
+
+print "\n";
+
+# Open the log file.
 my $outputFile = 'log.txt';
-open(my $fh, '<:encoding(UTF-8)', $inputFile)
-  or die "Could not open file '$inputFile' $!";
 open(my $fhOut, '>', $outputFile)
   or die "Could not open file '$outputFile' $!";
 
 
-print "Reading text files...\n";
-
+print "Reading text files...\n" if $verbose ;
 
 my $numLines = 0;
-# Main loop to build the ngram model. Iterates through each row in the line in the file. Stop when we reach the end of the input file.
-while (my $row = <$fh>) {
 
-  # Perform cleanup for each line.
-  # Remove white space at edges.
-  chomp $row;
+for (my $i=2; $i<$argCount; $i++) {
+  open(my $fh, '<:encoding(UTF-8)', $ARGV[$i])
+    or die "Could not open file '$ARGV[$i]' $!";
 
-  # Set everything to lower case.
-  $row = lc($row);
+  # Main loop to build the ngram model. Iterates through each row in the line in the file. Stop when we reach the end of the input file.
+  while (my $row = <$fh>) {
 
-  # Remove anything that is not a letter, number, or punctuation (, . ! ?). Accomplishes this by replacing other characters with a space.
-  $row =~ s/[^a-zA-Z0-9,.!?]/ /g;
+    # Perform cleanup for each line.
+    # Remove white space at edges.
+    chomp $row;
 
-  # Make sure that punctuation is seperated from words by a space.
-  $row =~ s/([,.!?])/ $1 /g;
+    # Set everything to lower case.
+    $row = lc($row);
 
-  # Replace all white space with a single space. This is done so we can split the line into tokens by spaces, then append tokens to the tokenArray.
-  $row =~ s/\s+/ /g;
-  my @tokens = split(/\s+/, $row);
-  push (@tokenArray, @tokens);
-  $numLines++;
+    # Remove anything that is not a letter, number, or punctuation (, . ! ?). Accomplishes this by replacing other characters with a space.
+    $row =~ s/[^a-zA-Z0-9,.!?]/ /g;
+
+    # Make sure that punctuation is seperated from words by a space.
+    $row =~ s/([,.!?])/ $1 /g;
+
+    # Replace all white space with a single space. This is done so we can split the line into tokens by spaces, then append tokens to the tokenArray.
+    $row =~ s/\s+/ /g;
+    my @tokens = split(/\s+/, $row);
+    push (@tokenArray, @tokens);
+    $numLines++;
+  }
+
+  # We have to make sure the token list ends in a sentence terminator, or BAD THINGS can happen.
+  my $numTokens = scalar @tokenArray;
+  if( $numTokens == 0) {
+    die "No tokens were able to be read from the corpus.";
+  }
+  elsif( $tokenArray[-1] !~ /[.!?]/g ) {
+    push(@tokenArray, ".");
+  }
+
 }
 
+# Print out information about the tokenization process.
 my $numTokens = scalar @tokenArray;
-print "\t$numLines lines read.\n";
-print "\t$numTokens tokens found.\n";
+print "\t$numLines lines read.\n" if $verbose;
+print "\t$numTokens tokens found.\n" if $verbose;
 
 
-print "Building n-gram model...\n";
-
+print "Building n-gram model...\n" if $verbose;
 
 # This variable contains the history as we go through the tokens. Since the list of tokens starts with a new sentence, history is first intialized with START_TAG.
 my @history = ($START_TAG);
 my $reset = 0;
+
 # Iterate through each token, and add entries into the hash table.
 for my $token (@tokenArray) {
 
@@ -126,7 +161,7 @@ for my $token (@tokenArray) {
     $hash{$history}{$token} = 1;
   }
 
-  # If reset is set to 1, we have reached the end of the sentence. Rset the history, and set reset = 0.
+  # If reset is set to 1, we have reached the end of the sentence. Reset the history, and set reset = 0.
   if( $reset == 1 ) {
     @history = ($START_TAG);
     $reset = 0;
@@ -135,33 +170,25 @@ for my $token (@tokenArray) {
   }
 }
 
-my $hashSize = scalar %hash;
-print "\t$hashSize entries in the hash table.\n";
 
-# Print out hash for testing
-# foreach my $historyKey (sort keys %hash) {
-#   foreach my $tokenKey (keys %{ $hash{$historyKey} }) {
-#     my $huhlength = length $historyKey;
-#     print $fhOut "$historyKey->$tokenKey:$hash{$historyKey}{$tokenKey}\n";
-#   }
-# }
-# foreach my $historyKey (sort keys %hash) {
-#   print $fhOut $historyKey."\n";
-# }
+my $hashSize = scalar %hash;
+print "\t$hashSize entries in the hash table.\n" if $verbose;
 
 # Store the number of sentences we will need total.
-my $numSentences = $m;
+my $sentenceId = 1;
 
-print "Generating sentences...\n\n";
+print "Generating sentences...\n\n" if $verbose;
 
 # Generate sentences.
-while($m > 0) {
+while($sentenceId <= $m) {
+
+  print "$sentenceId) ";
 
   # For each new sentence, start with just the START_TAG.
   @history = ($START_TAG);
   
-  my $sentenceId = $numSentences - $m + 1;
-  print "$sentenceId) ";
+  # Tag to indicate that we are on the first word of the sentence.
+  my $first = 1;
 
   # Loop until we come to a STOP_TAG.
   while(1) {
@@ -184,8 +211,10 @@ while($m > 0) {
     # Pick a random number, less than or equal to the frequency, to choose which word to use next.
     my $randomIndex = int(rand($historyFrequency)) + 1;
 
-    # Iterate through the tokens and counts until we find the correct word.
     my $word = "";
+        
+    # Iterate through the tokens and counts until we find the correct word. We calculate this
+    # by decrementing randomIndex by the frequency of each token of the hash until randomIndex <= 0;
     foreach my $tokenKey ( keys %{ $hash{$historyKey} } ) {
       $randomIndex -= $hash{$historyKey}{$tokenKey};
 
@@ -195,14 +224,15 @@ while($m > 0) {
       }
     }
 
+    # If we have reached the end of the sentence ($STOP_TAG) then end with a period, and newline.
     if( $word eq $STOP_TAG ) {
       print ".\n";
       last;
     }
+    # Otherwise, print out the word. Special cases of spacing for commas and capitalization for the first word.
     else {
-      my $historyLength = scalar @history;
-
-      if($historyLength == 1) {
+      if($first == 1) {
+        $first = 0;
         my $something = ucfirst $word;
         print "$something";
       }
@@ -214,21 +244,11 @@ while($m > 0) {
       }
     }
 
+    # Append the word to the history.
     push(@history, $word);
   }
 
-  $m--;
-
+  $sentenceId++;
 }
 
 print "\n";
-
-# TODO: make it so nonsense words are not added somehow.
-# TODO: make it so commas and other punctuation sit correctly in terms of spaces.
-# BUG (SOLVED): commas are being choses as first words in a sentence.
-  # Being caused by difficulties in the corpus
-  # i.e. Sr., Toronto
-# BUG (SOLVED): multiple spaces are being placed at a time, especially at the beginning of a sentence.
-# BUG (SOLVED): history keys are being stored which are less than n-1 tokens, and which don't start with a START_TAG.
-  # actually, all keys being stored are 3 tokens, but some tokens are just empty strings ( why? )
-  # caused by '[' symbol at least
